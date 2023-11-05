16f65e9425427b0050d7d1bdc6b034e741cb46f8
AnushervonKhidirov <ankhid98@gmail.com>
2023-11-05 20:58:31 +0500

    popup slice removed

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import popupSlice from './popup/slice'
 
 export const store = configureStore({
-    reducer: {
-        popup: popupSlice,
-    },
+    reducer: {},
 })
 
 export type RootState = ReturnType<typeof store.getState>
