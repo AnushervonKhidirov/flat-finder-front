ce19fa3fbbebb67150edf301ae15892ae77fcf99
AnushervonKhidirov <ankhid98@gmail.com>
2023-11-06 19:10:17 +0500

    ref removed from form component

--- src/components/common/form/form.tsx
+++ src/components/common/form/form.tsx
@@ -1,22 +1,16 @@
 import type { FC, FormEvent } from 'react'
 import type { IFormInputs, IConvertedFormData } from '@utils/types/form'
 
-import { useRef } from 'react'
-
 import Input from '../input/input'
 import Button from '../button/Button'
 
 import styles from './form.module.css'
 
 const Form: FC<IFormInputs> = ({ inputs, submitText, callback }) => {
-    const formElem = useRef<HTMLFormElement | null>(null)
-
     function submitForm(e: FormEvent<HTMLFormElement>) {
         e.preventDefault()
 
-        if (!formElem.current) return
-
-        const formData = new FormData(formElem.current)
+        const formData = new FormData(e.currentTarget)
         const dataToSubmit: IConvertedFormData = {}
 
         for (const input of formData.entries()) {
@@ -27,7 +21,7 @@ const Form: FC<IFormInputs> = ({ inputs, submitText, callback }) => {
     }
 
     return (
-        <form className={styles.form} ref={formElem} onSubmit={e => submitForm(e)}>
+        <form className={styles.form} onSubmit={e => submitForm(e)}>
             {inputs.map(input => (
                 <Input name={input.name} type={input.type} placeholder={input.placeholder} key={input.name} />
             ))}
