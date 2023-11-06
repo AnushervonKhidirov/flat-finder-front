c59a56d7525123cf0a4580ae418f8ea63afa424f
AnushervonKhidirov <ankhid98@gmail.com>
2023-11-06 19:11:08 +0500

    next auth added

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,26 @@
+import type { AuthOptions, User } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: 'credentials',
+            name: 'Credentials',
+            type: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'email' },
+                password: { label: 'password', type: 'password' },
+            },
+            async authorize(credentials) {
+                const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+                console.log(user)
+
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            },
+        }),
+    ],
+}
