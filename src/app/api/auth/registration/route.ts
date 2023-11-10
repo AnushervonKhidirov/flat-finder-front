1e4662640470dc5e006a2fe29131cdc1a197b95f
AnushervonKhidirov <ankhid98@gmail.com>
2023-11-10 23:11:22 +0500

    add connection to db insede the class

--- src/app/api/auth/registration/route.ts
+++ src/app/api/auth/registration/route.ts
@@ -3,18 +3,18 @@ import type { UserForm } from '@utils/types/user'
 import { NextResponse } from 'next/server'
 import { hash } from 'bcrypt'
 
-import { Query } from '@helpers/db-query/db-query'
+import { UserQuery } from 'lib/database'
 
 export async function POST(request: Request) {
     try {
         const requestBody: UserForm = await request.json()
         requestBody.password = await hash(requestBody.password, 0)
 
-        const isEmailContain = await Query.find(`email = '${requestBody.email}'`)
+        const isEmailContain = await UserQuery.find(`email = '${requestBody.email}'`)
 
         if (isEmailContain.length) throw new Error('Email is already exits')
 
-        await Query.insert(requestBody)
+        await UserQuery.insert(requestBody)
 
         return NextResponse.json({ message: "Now you're our user" })
     } catch (err: any) {
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,23 +1,78 @@
-import type { Connection } from 'mysql'
+import type { Connection, MysqlError } from 'mysql'
 import { createConnection } from 'mysql'
 
-let connection: Connection | null = null
+let savedConnection: Connection | null = null
 
-export default function getConnection(): Connection {
-    if (connection) return connection
+class dbQuery {
+    connection: Connection
+    table: string
 
-    const database = createConnection({
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-    })
+    constructor(table: string) {
+        this.connection = savedConnection ? savedConnection : this.connect()
+        this.table = table
+    }
 
-    try {
-        database.connect()
-        connection = database
-        return database
-    } catch (err: any) {
-        return err
+    async insert(data: {}) {
+        return await this.query(
+            `INSERT INTO ${this.table} (${this.prepareFields(data, 'key')})
+            VALUES (${this.prepareFields(data, 'value')});`,
+        )
+    }
+
+    async find(condition: string) {
+        return (await this.query(`SELECT * FROM ${this.table} WHERE ${condition};`)) as []
+    }
+
+    async query(query: string) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, (err: MysqlError, result: any) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+        })
+    }
+
+    prepareFields<T>(data: T, get?: 'key' | 'value') {
+        const arrData: string[] = []
+
+        for (let key in data) {
+            const skip = key === 'id' || key === 'repeat_password' || data[key] === ''
+
+            if (skip) continue
+
+            if (get === 'key') {
+                arrData.push(key)
+                continue
+            }
+
+            if (get === 'value') {
+                arrData.push(`'${data[key]}'`)
+                continue
+            }
+
+            arrData.push(`${key} = '${data[key]}'`)
+        }
+
+        return arrData.join(', ')
+    }
+
+    connect() {        
+        const database = createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+        })
+
+        try {
+            database.connect()
+            savedConnection = database
+            return database
+        } catch (err: any) {
+            return err
+        }
     }
 }
+
+export const UserQuery = new dbQuery(process.env.USER_TABLE!)
+// export const OfferQuery = new dbQuery(process.env.OFFER_TABLE!)
